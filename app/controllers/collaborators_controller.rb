c436bd2c4c8aca3ee3bb9ceb81bd507ec02e13e4
Lucas Perez <lucastperez@gmail.com>
2020-08-29 18:48:27 -0300

    Colaborador preenche perfil

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,13 +1,27 @@
 class CollaboratorsController < ApplicationController
   def show
+    @collaborator = Collaborator.find(params[:id])
   end
 
   def edit
-    @collaborator = current_collaborator
+    @collaborator = Collaborator.find(params[:id])
   end
 
   def update
-    @collaborator = current_collaborator
-    @collaborator.save
+    @collaborator = Collaborator.find(params[:id])
+    if @collaborator.update(collaborator_params)
+      flash[:notice] = 'Perfil preenchido com sucesso!' if @collaborator.profile_filled?
+      redirect_to @collaborator
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def collaborator_params
+    params.require(:collaborator)
+          .permit(:full_name, :social_name,
+                  :birth_date, :position, :sector)
   end
 end
--- spec/features/collaborator_fills_profile_spec.rb
+++ spec/features/collaborator_fills_profile_spec.rb
@@ -4,13 +4,14 @@ feature 'Collaborator fills profile' do
   scenario 'successfully' do
     user = Collaborator.create!(email:'user@email.com', password:'123456')
 
+    #TODO entender por que o comando "login_as" não ta funcionando
+    #login_as(user, scope: :collaborator? :user?)
     visit root_path
-    #TODO entender por que o comando login_as não ta funcionando
     fill_in 'Email', with: user.email
     fill_in 'Senha', with: user.password
     click_on 'Login'
-    #############################################################
-    click_on 'Preencha o seu perfil agora!'
+    click_on 'Perfil'
+    click_on 'Editar informações'
     fill_in 'Nome Completo', with: 'Usuário Colaborador'
     fill_in 'Nome Social', with: 'User'
     fill_in 'Data de Nascimento', with: '19/09/1995'
@@ -18,11 +19,26 @@ feature 'Collaborator fills profile' do
     fill_in 'Setor', with: 'Um Setor'
     click_on 'Enviar'
 
-    puts "============"
-    puts user.full_name
-
     expect(page).to have_content('Perfil preenchido com sucesso!')
-    #TODO fazer o capybara procurar o negócio vermelho
-    #expect(page).to_not have_css('style="color:red"', text: 'Perfil')
+    expect(page).to have_content('Seus anúncios')
+    #TODO fazer o capybara procurar o negócio vermelho nos dois cenários!
+    #expect(page).to_not have_css('style=color:red')
+  end
+
+  scenario 'did not fill' do
+    user = Collaborator.create!(email:'user@email.com', password:'123456')
+
+    visit root_path
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Login'
+    click_on 'Perfil'
+    click_on 'Editar informações'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Para que você possa anunciar produtos e efetivar a compra de produtos,')
+    expect(page).not_to have_content('Perfil preenchido com sucesso!')
+    expect(page).not_to have_content('Seus anúncios')
+    #expect(page).to have_css vermelho!
   end
 end
